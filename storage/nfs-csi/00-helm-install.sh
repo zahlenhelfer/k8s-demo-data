201149b4f0ac3064c1cc850c1a239f119bada49c
Marcus Ross <marcus.ross@zahlenhelfer.de>
2025-05-20 08:07:17 +0200

    add: nfs-csi install via helm and setting default-sc

---
+++ storage/nfs-csi/00-helm-install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
+helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs --namespace kube-system --version 4.11.0
